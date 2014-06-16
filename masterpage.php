334b9d0f498a00527fea84dd5d8cff529d116d1d
ThuanNH <nguyenhoangthuan.tn@gmail.com>
2014-06-16 14:23:23 +0700

    wqe

---
+++ masterpage.php
@@ -0,0 +1,61 @@
+<head>
+
+	<link href="css/masterStyle.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<section id="header" > 
+		<p>header goes here !!! </p>
+		<p>header goes here !!! </p>
+		<p>header goes here !!! </p>
+		<p>header goes here !!! </p>
+	</section>
+	<section id="mainMenu" > 
+		<p>menu goes here !!! </p>
+		<p>menu goes here !!! </p>
+	</section>
+	<section id="topAds" > 
+		<p>Top Ads go here !!! </p>
+		<p>Top Ads go here !!! </p>
+		<p>Top Ads go here !!! </p>
+	
+	</section>
+	<section id="mainContent" > 
+		<section id="leftContent" > 
+		
+			<section id="slideshowBox" > 
+				<p>The slideshow goes here !!! </p>
+			</section>
+			<section id="playlistBox" > 
+				<p>The playlist box goes here !!! </p>
+			</section>
+			<section id="ColsVideoBox" > 
+				<p>The 2 collumns video box goes here !!! </p>
+			</section>
+			<section id="videoBox" > 
+				<p>The video box goes here !!! </p>
+			</section>
+			<section id="songBox" > 
+				<p>The song box goes here !!! </p>
+			</section>
+			<div style="clear:both"></div>
+		</section>
+		<section id="rightContent" > 
+		
+			<section id="sideAds" > 
+				<p>The side ads box goes here !!! </p>
+			</section>
+			<section id="sideMusicBox" > 
+				<p>The side music box goes here !!! </p>
+			</section>
+			<section id="sideHotTopicBox" > 
+				<p>The side Hot Topic box goes here !!! </p>
+			</section>
+			<section id="sideSingerBox" > 
+				<p>The singer box goes here !!! </p>
+			</section>
+			<div style="clear:both"></div>
+		</section>
+		<div style="clear:both"></div>
+	</section>
+	<section id="footer" > <p>footer goes here !!! </p> </section>
+</body>
