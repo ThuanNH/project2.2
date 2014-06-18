045ed15f97d6405ed11b31dab6878bc0b6d06ae4
NguyenTrongAn <bebeobe851@yahoo.com.vn>
2014-06-18 17:01:21 +0700

    File ẩn

--- masterpage.php
+++ masterpage.php
@@ -13,6 +13,7 @@
 		<p>header goes here !!! </p>
 		<p>header goes here !!! </p>
 		<p>header goes here !!! </p>
+		code masterpage.php dau ?
 	</section>
 	<section id="mainMenu" > 
 		<p>menu goes here !!! </p>
@@ -63,3 +64,173 @@
 	</section>
 	<section id="footer" > <p>footer goes here !!! </p> </section>
 </body>
+<?php
+/*
+<html>
+<meta  http-equiv=" Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css"  medial="all" href="file.css">
+<form>
+<body>
+<div class="top-header">
+<div class="wpage">
+<a class="logo" href="DeTai.php"></a>
+<div class="box_search">
+<input type='Text' class="i-search" autofocus="" autocomplete="off"  value="" maxlength="45" placeholder="Tìm bài hát, playist,ca sĩ, video"></input>
+<input id="btnSearch" class="b-search" type="submit" value=" "></input></div>
+<div class="box_user_login">
+<a href="" color=blue class="none-line">Đăng Nhập </a> |
+<a href=""  color=blue class="none-line">Đăng ký</a></div>
+</div>
+</div>
+<div id="header" class="header">
+<div class="wpage">
+<div class="menu-top">
+<ul id="menuTop" class="notifi">
+    <li id="icon_menu_logo" class="icon_logo_menu hide"></li>
+	<li id="icon_menu_home" class="active">
+	<a href="DeTai.php"><img src="images/home.jpg"></a></li>
+	<li class="">Bài hát</li>
+	<li class="">Playist</li>
+	<li class="">Video</li>
+	<li class="">Bảng xếp hạng</li>
+</ul>
+</div>
+</div>
+</div>
+<div >
+<embed   class="posi" src="images/clip.swf" wmode="transparent">
+</embed></div>
+<div class="center">
+<div class="left">
+<div class="slideshow" >
+	<div><a href="#"><img src="images/show1.jpg" alt="" border="0" /></a></div>
+	<div><a href="#"><img src="images/show2.jpg" alt="" border="0" /></a></div>
+	<div><a href="#"><img src="images/show3.jpg" alt="" border="0" /></a></div>
+	<div><a href="#"><img src="images/show4.jpg" alt="" border="0" /></a></div>
+</div>
+<div class="show">
+<ul id="anh">
+<li rel="1"	 class="active"> 
+<img alt="" src="images/show1.jpg" width="138px" height="100px"></img>
+</li>
+<li rel="2" > 
+<img alt="" src="images/show2.jpg" width="138px" height="100px"></img>
+</li>
+<li rel="3"> 
+<img alt="" src="images/show3.jpg" width="138px" height="100px"/>
+</li>
+<li rel="4"> 
+<img alt="" src="images/show4.jpg" width="138px" height="100px"/>
+</li>
+</ul>
+</div></div>
+<div class="right">
+<div >
+<embed   class="posi1" src="images/clip3.swf" wmode="transparent">
+</embed></div>
+</br>
+<div class="green">
+<label> Bảng xếp hạng</label>
+<img src="images/play.jpg"/> 
+<select class="right">
+<option >Việt Nam</option>
+<option >Châu Á</option>
+<option >Âu Mỹ</option></select>
+</div>
+ <div >
+ <div class="one"> 1</div>
+ <div >
+ <a class="one2"> Yêu em là Định Mệnh</a><a class="one3" ><img src="images/listen.jpg"/>1231313</a>
+ </br>
+ <a class="one4"> Cao Thái Sơn</a>
+ </div>
+ </div>
+ </br></br>
+ <div class="chude">
+ Chủ đề hot
+ </div>
+<div><img src="images/chude1.jpg"/>
+</br>
+<a href="#" class="style-none">Nhạc tuyển tập</a>
+</br>
+Xin hân hạnh gửi đến các bạn</br> những tuyển tập các ca khúc hay</br>
+ nhất của những nghệ sĩ</br> được yêu thích nhất trên 
+ website</br> NhacCuaTui.Com. Mời các</br> bạn cùng lắng nghe và chia sẻ</br><a href="#" class="style-none">Xem Thêm</a>
+	</div>
+	</br>
+	<div><img src="images/chude2.jpg"/>
+	</br>
+<a href="#" class="style-none">Nhạc trữ tình chọn lọc</a>
+</br>
+Tuyển tập nhạc trữ tình tuyển</br> chọn những playlist nhạc trữ</br> tình hay
+ nhất với những giọng ca</br> tài danh hàng đầu sẽ đưa</br> quí vị trở về với khung trời
+ hoa</br> bướm ngày xưa. Mời các</br> bạn cùng thưởng...<a href="#" class="style-none">Xem Thêm</a>
+	</div>
+	
+	<a href="#" class="style-none">Nhạc bất hủ-Retro</a>
+	</br>
+	<a href="#" class="style-none">Song Ca</a>
+	</br>
+	<a href="#" class="style-none">Nhạc sàn mạnh</a>
+	<div class="casi">
+	Ca Sĩ / Nghệ Sĩ
+	</div>
+	<ul>
+	<li >
+	<img src="images/lbh.jpg"/>
+	<a> Lương Bích Hữu</a>
+	</li>
+	<img src="images/hqh.jpg"/>
+	<a> Hồ Quang Hiếu</a>
+	</ul>
+</div>
+</div>
+<div class="footer">
+<div class="wpage">
+
+    <div class="mfooter">
+	<ul class="menu">
+        <li class="range">
+			Phiên bản khác
+			<ul>
+			<li>
+			<label>
+    Mobile:
+</label>
+<a href="#" class="style-none1">Iso</a>,
+<a href="#" class="style-none1">Android</a>,</br>
+<a href="#" class="style-none1">Windows</a>,
+<a href="#" class="style-none1">Nokia</a>
+</li>
+</br>
+<li><label>Khác</label>
+<a href="#" class="style-none1">Windows</a>,
+<a href="#" class="style-none1">Smart TV</a>
+</li></ul></li>
+<li class="range">
+Kết nối với chúng tôi</br>
+<a href="#" class="style-none1">Twitter</a></br>
+<a href="#" class="style-none1">Facebook</a></br>
+<a href="#" class="style-none1">Linkhey</a></br>
+<a href="#" class="style-none1">Google</a>
+</li >
+<li class="range">Hỗ Trợ</br>
+<a href="#" class="style-none1">Quảng cáo</a></br>
+<a href="#" class="style-none1">Trợ giúp</a></br>
+<a href="#" class="style-none1">Sơ đồ</a></br>
+<a href="#" class="style-none1">Thỏa thuận</a>
+</li>
+<li class="range">
+Liên kết & Hợp Tác </br>
+<a href="#" class="style-none1">Viss.vn</a>- Làm quen kết bạn</br>
+<a href="#" class="style-none1">Nivi.vn</a>-Chơi game,xem phim</br>
+<a href="#" class="style-none1">Nava.vn</a>-Mua sắm online
+</li>
+			</ul>
+			</div>
+			</div>
+</div>
+</body>
+</form></html>
+*/
+?>
