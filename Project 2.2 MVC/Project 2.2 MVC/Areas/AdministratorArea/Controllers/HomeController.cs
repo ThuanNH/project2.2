29891f163df3091718afce0410e6fc5fbbeecd0d
thanhhv26 <thanhhv26@gmail.com>
2014-08-18 10:27:46 +0700

    - test

---
+++ Project 2.2 MVC/Project 2.2 MVC/Areas/AdministratorArea/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_2._2_MVC.Areas.AdministratorArea.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /AdministratorArea/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file) {
+
+            string utl = util.getlyricRootPath(file.FileName);
+
+            List<HttpPostedFileBase> files = new List<HttpPostedFileBase>();
+            files.Add(file);
+            util.UploadFile(files,constants.lyricDir);
+            ViewBag.Title = utl;
+            return View();
+        }
+
+    }
+}
